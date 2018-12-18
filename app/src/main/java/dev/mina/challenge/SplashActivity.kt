e6ad75060160cead6f8be84d8c56888aa97bd580
Mina Fayez <Mina.Fayez@protonmail.com>
2018-12-18 17:14:45 +0200

    App Creation

---
+++ app/src/main/java/dev/mina/challenge/SplashActivity.kt
@@ -0,0 +1,12 @@
+package dev.mina.challenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
